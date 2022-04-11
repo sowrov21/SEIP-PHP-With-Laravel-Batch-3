1c0636fff36412536a863b10e36429beb4bdb901
sowrov21 <sowrov.it@gmail.com>
2022-04-11 12:52:45 +0600

    Class 28 addded

---
+++ Class-28/database.php
@@ -0,0 +1,18 @@
+<?php
+//echo "Database";
+
+    $dbDriver = 'mysql';
+    $dbHost   = 'localhost';
+    $dbPort   = '3306';
+    $dbName   = 'inventory';
+    $dbUser   = 'root';
+    $dbPass   = 'root';
+
+try
+{
+
+}
+catch()
+{
+
+}
