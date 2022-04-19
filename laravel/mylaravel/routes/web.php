6b811bff2645aca20c0521f7638e2ee269ecdbc4
sowrov21 <sowrov.it@gmail.com>
2022-04-19 12:20:24 +0600

    Laravel app added

---
+++ laravel/mylaravel/routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('dashboard');
+});
+
+
+Route::get('/button', function () {
+    return view('button');
+})->name('button');
+
+
+Route::get('/card', function () {
+    return view('card');
+})->name('card');
+
+
+
+
+
